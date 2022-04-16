be0143ad2632809fbbae374cd8ccf1032c055187
Dan <dan@evolution7.com.au>
2022-04-16 11:23:57 +0200

    ..

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', async (req, res) => {
+app.get('/api/test', async (req, res) => {
   res.send('TEST...');
 });
 
